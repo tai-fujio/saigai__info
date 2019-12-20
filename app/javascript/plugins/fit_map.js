a459d2b003628aa0a1bfcb62668498a08a5a52a8
T.Fujio <fujio@MacBookPro.local>
2019-12-20 17:37:36 +0900

    fit_map.js作成

---
+++ app/javascript/plugins/fit_map.js
@@ -0,0 +1,9 @@
+const fitMap = (map, markers) => {
+  var center_marker = markers.slice(1)[0];
+  var home_coordinates = [
+    [center_marker.lng,center_marker.lat],[center_marker.lng,center_marker.lat]
+  ];
+  map.fitBounds(home_coordinates, { padding: 70, maxZoom: 17, duration: 0 });
+};
+
+export {fitMap};
