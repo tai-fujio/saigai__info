a901e3f3b0fe3edcbdefd3db6b3f6ef3da8a260a
T.Fujio <fujio@MacBookPro.local>
2019-12-20 17:35:44 +0900

    register_site.js作成

---
+++ app/javascript/plugins/register_site.js
@@ -0,0 +1,13 @@
+document.getElementById("map").onclick = function(e){
+  var coordinates = document.getElementsByTagName("pre").info.innerHTML
+  var lng_index = coordinates.indexOf("lng");
+  var lat_index = coordinates.indexOf("lat");
+  var lng = parseFloat(coordinates.substr(lng_index+5,14));
+  var lat = parseFloat(coordinates.substr(lat_index+5,15))
+  for(var i = 0; i < $('[id=site_latitude]').length; i++){
+    $('[id=site_latitude]')[i].value = lat;
+  };
+  for(var i = 0; i < $('[id=site_longitude]').length; i++){
+    $('[id=site_longitude]')[i].value = lng;
+  };
+}
