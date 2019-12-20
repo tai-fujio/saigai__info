c089307b2b1ad17a8db2d927bcabb2322735a7a5
T.Fujio <fujio@MacBookPro.local>
2019-12-20 15:06:54 +0900

    application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery.turbolinks
 //= require rails-ujs
-//= require activestorage
-//= require turbolinks
+//= ./node_modules/jquery/dist/jquery.js
+//= require mapbox-gl
 //= require_tree .
