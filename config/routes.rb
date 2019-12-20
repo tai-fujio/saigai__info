03744a5aea405e34354eaa48f749789cde3787c3
T.Fujio <fujio@MacBookPro.local>
2019-12-20 17:52:34 +0900

    routes.rb追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: 'maps#index'
+  get 'votes/count'
   resources :maps do
+    resources :sites do
+      resources :reviews do
+      end
+    end
   end
 end
