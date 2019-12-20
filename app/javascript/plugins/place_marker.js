56a508b235d71bc28c2991964ec65cc9fb12d2d9
T.Fujio <fujio@MacBookPro.local>
2019-12-20 17:34:52 +0900

    place_marker.js作成

---
+++ app/javascript/plugins/place_marker.js
@@ -0,0 +1,16 @@
+const placeMarker = (map, markers) => {
+
+  if (markers && Array.isArray(markers) ) {
+    markers.forEach((marker) => {
+    var element = document.createElement('a');
+    element.setAttribute(`href`,`${marker.map_id}/sites/${marker.id}`);
+    element.setAttribute(`data-remote`,`true`);
+    element.className = marker.name;
+    new mapboxgl.Marker(element)
+      .setLngLat([ marker.lng, marker.lat])
+      .addTo(map);
+    });
+  };
+};
+
+export { placeMarker };
