401e34afeb87a2dd909e6d9c0d3746317769a4a4
T.Fujio <fujio@MacBookPro.local>
2019-12-20 16:49:55 +0900

    maps#show,maps#compose,maps#sites記述

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,3 +1,32 @@
 class MapsController < ApplicationController
-  def index; end
+
+  def sites
+    @sites ||= @map.sites
+  end
+
+  def compose(sites)
+    sites.map do |site|
+    {lat: site.latitude,
+    lng: site.longitude,
+    name: site.name,
+    id: site.id,
+    map_id: site.map_id
+    }
+    end
+  end
+
+  def index
+    @maps = Map.all
+  end
+
+  def show
+    @map = Map.find(params[:id])
+    @site = @map.sites.build
+    sites
+    @all_sites = compose(@sites)
+  end
+  def new;end
+  def create;end
+  def destroy;end
+
 end
