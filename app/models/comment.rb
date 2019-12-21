ed7d436eb026c5c80bfef88363dec406a0f14d79
T.Fujio <fujio@MacBookPro.local>
2019-12-21 20:51:52 +0900

    コメント入力欄css調整

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :site
   has_one :review ,dependent: :destroy
+  default_scope { order(created_at: :desc) }
+  validates :content, length:{maximum:140}
 end
