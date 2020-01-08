714578ad8d99b2d19b369804f6c5d4989ec71d1d
T.Fujio <fujio@MacBookPro.local>
2020-01-08 15:40:06 +0900

    icon追加、縮尺修正

--- app/models/site.rb
+++ app/models/site.rb
@@ -6,11 +6,4 @@ class Site < ApplicationRecord
   validates :latitude, presence: true, uniqueness:{ scope: [:longitude] }
   validates :longitude, presence: true, uniqueness:{ scope: [:latitude] }
   validates :title, presence: true,length:{maximum:20}, on: :update
-  before_destroy :cannot_delete_first_site
-
-  def cannot_delete_first_site
-    @site = Site.find(id)
-    @map = Map.find(map_id)
-    throw(:abort) if @site == @map.sites.first
-  end
 end
