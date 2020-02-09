cccdb1940e28478f49a116d90860a30e77dadb91
tai-fujio <taisaburo.fujio@gmail.com>
2020-02-10 02:19:32 +0900

    レスポンシブ不具合修正

--- app/javascript/plugins/register_site.js
+++ app/javascript/plugins/register_site.js
@@ -1,39 +1,84 @@
 const registerSite = () => {
 
 if(document.getElementById("map")){
-document.getElementById("map").ondblclick = function(e){
+  //タブレットより小さい場面サイズの場合
+  if (window.matchMedia('(max-width: 767px)').matches) {
+    var tapCount = 0 ;
+    document.getElementById("map").addEventListener("touchstart", function(e){
+    // ダブルタップが行われたかどうかの判定
+    if( !tapCount ) {
+      ++tapCount ;
+      setTimeout(function(){
+        tapCount = 0;
+      },300) ;
+    }else{
+      var modal = document.createElement("div");
+      modal.className = "modal"
+      var modalContent = document.createElement("div"); 
+      modalContent.className = "modal-content";
+      var modalInfo = document.createElement("div");
+      modalInfo.className = "modal-info";
+  
+      modalInfo.appendChild(document.createTextNode('この地点を登録できます'));
+      modalInfo.appendChild(document.createElement('br'));
+      modalInfo.appendChild(document.createTextNode('アイコンから選んでください'));
+      modalContent.appendChild(modalInfo);
+      modal.appendChild(modalContent);
+      document.body.appendChild(modal);
+      
+      setTimeout(function(){
+        document.body.removeChild(modal);
+      },900);
+  
+      var coordinates = document.getElementsByTagName("pre")
+      .info.innerHTML
+      var lng_index = coordinates.indexOf("lng");
+      var lat_index = coordinates.indexOf("lat");
+      var lng = parseFloat(coordinates.substr(lng_index+5,16));
+      var lat = parseFloat(coordinates.substr(lat_index+5,17))
+      for(var i = 0; i < $('[id=site_latitude]').length; i++){
+        $('[id=site_latitude]')[i].value = lat;
+      };
+      for(var i = 0; i < $('[id=site_longitude]').length; i++){
+        $('[id=site_longitude]')[i].value = lng;
+      };
+      sessionStorage.setItem('dbclick', true);
+      }});
+    }else{
+    //タブレットより大きい場面サイズの場合
+    document.getElementById("map").ondblclick = function(){
+    var modal = document.createElement("div");
+    modal.className = "modal"
+    var modalContent = document.createElement("div"); 
+    modalContent.className = "modal-content";
+    var modalInfo = document.createElement("div");
+    modalInfo.className = "modal-info";
 
-  var modal = document.createElement("div");
-  modal.className = "modal"
-  var modalContent = document.createElement("div"); 
-  modalContent.className = "modal-content";
-  var modalInfo = document.createElement("div");
-  modalInfo.className = "modal-info";
+    modalInfo.appendChild(document.createTextNode('この地点を登録できます'));
+    modalInfo.appendChild(document.createElement('br'));
+    modalInfo.appendChild(document.createTextNode('アイコンから選んでください'));
+    modalContent.appendChild(modalInfo);
+    modal.appendChild(modalContent);
+    document.body.appendChild(modal);
+    setTimeout(function(){
+      document.body.removeChild(modal);
+    },900);
 
-  modalInfo.appendChild(document.createTextNode('この地点を登録できます'));
-  modalInfo.appendChild(document.createElement('br'));
-  modalInfo.appendChild(document.createTextNode('アイコンから選んでください'));
-  modalContent.appendChild(modalInfo);
-  modal.appendChild(modalContent);
-  document.body.appendChild(modal);
-  setTimeout(function(){
-    document.body.removeChild(modal);
-  },900);
-
-  var coordinates = document.getElementsByTagName("pre")
-  .info.innerHTML
-  var lng_index = coordinates.indexOf("lng");
-  var lat_index = coordinates.indexOf("lat");
-  var lng = parseFloat(coordinates.substr(lng_index+5,14));
-  var lat = parseFloat(coordinates.substr(lat_index+5,15))
-  for(var i = 0; i < $('[id=site_latitude]').length; i++){
-    $('[id=site_latitude]')[i].value = lat;
+    var coordinates = document.getElementsByTagName("pre")
+    .info.innerHTML
+    var lng_index = coordinates.indexOf("lng");
+    var lat_index = coordinates.indexOf("lat");
+    var lng = parseFloat(coordinates.substr(lng_index+5,16));
+    var lat = parseFloat(coordinates.substr(lat_index+5,17))
+    for(var i = 0; i < $('[id=site_latitude]').length; i++){
+      $('[id=site_latitude]')[i].value = lat;
+    };
+    for(var i = 0; i < $('[id=site_longitude]').length; i++){
+      $('[id=site_longitude]')[i].value = lng;
+    };
+    sessionStorage.setItem('dbclick', true);
+    }};
   };
-  for(var i = 0; i < $('[id=site_longitude]').length; i++){
-    $('[id=site_longitude]')[i].value = lng;
-  };
-  sessionStorage.setItem('dbclick', true);
-  }};
 };
 
-export {registerSite};
+  export {registerSite};
