6e7598a0b8eeedad1626a6c248a4a103e1f58991
tai-fujio <taisaburo.fujio@gmail.com>
2020-02-13 20:06:52 +0900

    画面レイアウト修正

--- app/javascript/plugins/right_bar_scroll.js
+++ app/javascript/plugins/right_bar_scroll.js
@@ -1,11 +1,7 @@
 const rightBarScroll = () => {
-  //タブレットより大きい場面サイズの場合スクロール時ヘッド固定
-  if (!window.matchMedia('(max-width: 767px)').matches) {
-    if (!window.matchMedia('(max-hight: 767px)').matches) {
     $("#right_bar").scroll(function(){
-      $("#right_bar_head").stop().animate({"top" : $("#right_bar").scrollTop()}, 100);
+      $("#right_bar_header").stop().animate({"top" : $("#right_bar").scrollTop()}, 100);
     });
-  }
-}};
+  };
 
 export { rightBarScroll };
