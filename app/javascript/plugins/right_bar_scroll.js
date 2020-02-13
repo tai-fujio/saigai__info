4859a1ee96a85f79b51552438b098e2c9dffc3f7
tai-fujio <taisaburo.fujio@gmail.com>
2020-02-13 19:06:10 +0900

    画面サイズによってヘッダ固定

--- app/javascript/plugins/right_bar_scroll.js
+++ app/javascript/plugins/right_bar_scroll.js
@@ -1,9 +1,11 @@
 const rightBarScroll = () => {
   //タブレットより大きい場面サイズの場合スクロール時ヘッド固定
   if (!window.matchMedia('(max-width: 767px)').matches) {
-  $("#right_bar").scroll(function(){
-    $("#right_bar_head").stop().animate({"top" : $("#right_bar").scrollTop()}, 100);
-  });
+    if (!window.matchMedia('(max-hight: 767px)').matches) {
+    $("#right_bar").scroll(function(){
+      $("#right_bar_head").stop().animate({"top" : $("#right_bar").scrollTop()}, 100);
+    });
+  }
 }};
 
 export { rightBarScroll };
