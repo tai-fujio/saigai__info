0461549f3dbc830cde2ebf273b841d1525d135f5
tai-fujio <taisaburo.fujio@gmail.com>
2020-02-13 14:49:26 +0900

    地図ブラウザ非対応時のアラート追記

--- app/javascript/plugins/init_map.js
+++ app/javascript/plugins/init_map.js
@@ -9,68 +9,72 @@ import { createMapNotify } from './create_map_notify'
 
 const initMap = () => {
   const mapDiv = document.getElementById('map');
-  if (mapDiv) {
-    mapboxgl.accessToken = process.env.MAPBOX_API_KEY
-    const markers = JSON.parse(mapDiv.dataset.markers);
-    if(markers.length <2){
-      // #サイト未登録の場合
-      window.map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-      });
-
-      map.flyTo({
-        center: [ 139.7, 35.6 ],
-        zoom: 10,
-      });
-
-      map.on('mousemove', function (e) {
-        document.getElementById('info').innerHTML =
-        JSON.stringify(e.lngLat.wrap());
-      });
-
-      map.addControl(new mapboxgl.GeolocateControl({
-      positionOptions: {
-      enableHighAccuracy: false,timeout:6000
-      },
-      showUserLocation: false
-      }));
-
-      createMapNotify();
-
+  if (!mapboxgl.supported()) {
+    alert('このブラウザは地図の表示に対応していません');
     }else{
-      // #サイトが登録してある場合
-      window.map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-      });
+    if (mapDiv) {
+      mapboxgl.accessToken = process.env.MAPBOX_API_KEY
+      const markers = JSON.parse(mapDiv.dataset.markers);
+      if(markers.length <2){
+        // #サイト未登録の場合
+        window.map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/streets-v11',
+        });
 
-      fitMap(map,markers);
+        map.flyTo({
+          center: [ 139.7, 35.6 ],
+          zoom: 10,
+        });
 
-      placeMarker(map, markers);
+        map.on('mousemove', function (e) {
+          document.getElementById('info').innerHTML =
+          JSON.stringify(e.lngLat.wrap());
+        });
 
-      map.on('mousemove', function (e) {
-        document.getElementById('info').innerHTML =
-        JSON.stringify(e.lngLat.wrap());
-      });
+        map.addControl(new mapboxgl.GeolocateControl({
+        positionOptions: {
+        enableHighAccuracy: false,timeout:6000
+        },
+        showUserLocation: false
+        }));
 
-      map.addControl(new mapboxgl.GeolocateControl({
-      positionOptions: {
-      enableHighAccuracy: true
-      },
-      trackUserLocation: true
-      }));
+        createMapNotify();
 
-      rightbarOpen();
+      }else{
+        // #サイトが登録してある場合
+        window.map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/streets-v11',
+        });
 
-      sidebarClose();
+        fitMap(map,markers);
 
-      centeringMap();
+        placeMarker(map, markers);
 
-      rightBarScroll();
+        map.on('mousemove', function (e) {
+          document.getElementById('info').innerHTML =
+          JSON.stringify(e.lngLat.wrap());
+        });
+
+        map.addControl(new mapboxgl.GeolocateControl({
+        positionOptions: {
+        enableHighAccuracy: true
+        },
+        trackUserLocation: true
+        }));
+
+        rightbarOpen();
+
+        sidebarClose();
+
+        centeringMap();
+
+        rightBarScroll();
+      };
     };
+    sessionStorage.setItem('dbclick', false);
   };
-  sessionStorage.setItem('dbclick', false);
 };
 
 export {initMap};
