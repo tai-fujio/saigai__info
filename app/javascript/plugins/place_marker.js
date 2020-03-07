011c8c710491e24395f49e80326a5ef8e3064968
tai-fujio <taisaburo.fujio@gmail.com>
2020-03-07 17:34:31 +0900

    地点検索結果にPopupで地名表示

--- app/javascript/plugins/place_marker.js
+++ app/javascript/plugins/place_marker.js
@@ -1,6 +1,7 @@
 const placeMarker = (map, markers) => {
 
   if (markers && Array.isArray(markers) ) {
+    if(markers[0].name != "fas fa-search fa-3x faa-flash animated"){
     var all_markers = markers
     markers.forEach((marker) => {
     // 最初に登録されたマーカーを点滅させる
@@ -16,6 +17,19 @@ const placeMarker = (map, markers) => {
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
     });
+  }else{
+    markers.forEach((marker) => {
+      var element = document.createElement('a');
+      element.className = marker.name;
+      var popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
+        `${marker.display_name}`
+        );
+      new mapboxgl.Marker(element)
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(map);
+      });
+    };
   };
 };
 
