fecbb237d862f63b8fbf3763b558348d88adcbdd
tai-fujio <taisaburo.fujio@gmail.com>
2020-03-09 02:05:03 +0900

    debugger削除

--- app/javascript/plugins/pre_map.js
+++ app/javascript/plugins/pre_map.js
@@ -4,7 +4,6 @@ $(".password_field").on({
   'mouseenter':function(){
     var text = $(this).attr('data-set');
     $(this).append('<div class="tooltips">'+text+'</div>');
-    debugger
   },
   'mouseleave':function(){
     $(this).find(".tooltips").remove();
